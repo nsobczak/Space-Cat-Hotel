2f11a2b8508ed838dc92cde999f6d699511a10eb
nsobczak <nicolas.sobczak@orange.fr>
2019-11-16 00:52:34 +0100

    Initialized ue4 project + rigged SpaceCat model

---
+++ SpaceCatRTS/Source/SpaceCatRTS/SpaceCatRTS.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceCatRTS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceCatRTS, "SpaceCatRTS" );
+
+DEFINE_LOG_CATEGORY(LogSpaceCatRTS)
+ 
